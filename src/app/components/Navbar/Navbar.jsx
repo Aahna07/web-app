08bada2d101c7c6184b91e8af4238a4e9a14c122
Aahna123 <500083440@stu.upes.ac.in>
2024-07-24 01:20:05 +0530

    "Navbar added"

---
+++ src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+"use client";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+
+import Toolbar from "@mui/material/Toolbar";
+
+import Typography from "@mui/material/Typography";
+
+import Container from "@mui/material/Container";
+
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+function ResponsiveAppBar() {
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            LOGO
+          </Typography>
+          <AddShoppingCartIcon />
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const page = () => {
-  return <div>hello1423</div>;
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
 };
 
 export default page;
